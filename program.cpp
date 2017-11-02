361c058e1c05b9b4c86b4cc244a2b3d852cf075c
markosokolovic7 <msokolovic21@gmail.com>
2017-11-02 11:20:57 +0100

    switch case kod

--- program.cpp
+++ program.cpp
@@ -2,5 +2,19 @@
 using namespace std;
 int main()
 {
+    cout<<"1.- Racunsko pojacalo u invertirajucoj zatvorenoj petlji"<<endl;
+    cout<<"2.-Intergrator "<<endl;
+    cout<<"3.-Derivator "<<endl;
+    cout<<"4.-Tezinsko zbrajalo "<<endl;
+    cout<<"5.-Racunsko pojacalo u neinvertirajucoj zatvorenoj petlji "<<endl;
+    cout<<"6.-Gustoca magnetskog toka(magnetska indukcija) "<<endl;
+    cout<<"7.- Jakost magnetskog polja unutar ravnog vodica "<<endl;
+    cout<<"8.- Stupanj iskoristivosti realnog stroja "<<endl;
+    cout<<"9.- Brzina vrtnje sinkronog stroja "<<endl;
+    cout<<"10.- Klizanje asinkronog motora"<<endl;
+    int odabir;
+    cout<<"Odaberite broj 1-10 s odgovarajucom formulom!"<<endl;
+    cin>>odabir;
+    switch (odabir)
     return 0;
 }

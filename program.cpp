b862e18ee52eeb51223a0c5e6d8c63aa5a8d8e8d
markosokolovic7 <msokolovic21@gmail.com>
2017-11-16 11:10:30 +0100

    prva 2 casea
    morarm pormjenit na gogle drive formule

--- program.cpp
+++ program.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 int main()
 {
-    cout<<"1.- Racunsko pojacalo u invertirajucoj zatvorenoj petlji"<<endl;
-    cout<<"2.-Intergrator "<<endl;
+    cout<<"1.- Ohmov zakon "<<endl;
+    cout<<"2.-Ukupni otpor paralelno spojenih otpora   "<<endl;
     cout<<"3.-Derivator "<<endl;
     cout<<"4.-Tezinsko zbrajalo "<<endl;
     cout<<"5.-Racunsko pojacalo u neinvertirajucoj zatvorenoj petlji "<<endl;
@@ -15,65 +15,71 @@ int main()
     int odabir;
     cout<<"Odaberite broj 1-10 s odgovarajucom formulom!"<<endl;
     cin>>odabir;
+    int U,R,
      switch(odabir)
     {
     case 1:
-        cout<<"Racunsko pojacalo u invertirajucoj zatvorenoj petlji";
-        cout<<""<<;
-        cin>>a;
-        cout<<""<<;
+        cout<<"Ohmov zakon";
+        cout<<"Upisite vrijednosti napona(U) "<<;
+        cin>>U;
+        cout<<"Upisite vrijednosti otopra(R)"<<;
+        cin>>R;
+        struja=U/R;
+        cout<<"Struja zadanih vrijednosti je: "<<struja<<endl;
         break;
     case 2:
-         cout<<"Intergrator";
-         cout<<""<<;
-        cin>>a;
-        cout<<""<<(a/7.56);
+         cout<<"Ukupni otpor 2 paralelno spojenih otpora  ";
+         cout<<"Upisite vrijednosti dva otpornika: "<<;
+        cin>>R1<<R2;
+        1/Ruk=1/R1+1/R2;
+        1/Ruk=Ukupni_otpor
+        cout<<"Otpor zadanih vrijednosti je: "<<Ukupni_otpor<<endl;
         break;
     case 3:
          cout<<"Derivator";
-         cout<<""<<;
+         cout<<"Upisite vrijednosti"<<;
         cin>>a;
         cout<<"Iznos u dolarima je: "<<a/7.02;
         break;
     case 4:
          cout<<"Tezinsko zbrajalo";
-         cout<<""<<;
+         cout<<"Upisite vrijednosti"<<;
         cin>>a;
         cout<<"Iznos u eurima je: "<<(9.01*a)/7.56;
         break;
     case 5:
          cout<<"Racunsko pojacalo u neinvertirajucoj zatvorenoj petlji";
-         cout<<""<<;
+         cout<<"Upisite vrijednosti"<<;
         cin>>a;
         cout<<"Iznos u dolarima je: "<<(9.01*a)/7.02;
         break;
     case 6:
          cout<<"Gustoca magnetskog toka(magnetska indukcija)";
-         cout<<""<<;
+         cout<<"Upisite vrijednosti"<<;
         cin>>a;
         cout<<"Iznos u dolarima je: "<<(a*7.02)/7.56;
         break;
     case 7:
          cout<<"Jakost magnetskog polja unutar ravnog vodica";
-         cout<<""<<;
+         cout<<"Upisite vrijednosti"<<;
         cin>>a;
         cout<<"Iznos u kunama je: "<<a*9.01;
         break;
     case 8:
         cout<<"Stupanj iskoristivosti realnog stroja";
-        cout<<""<<;
+        cout<<"Upisite vrijednosti"<<;
         cin>>a;
         cout<<"Iznos u kunama je: "<<a*7.02;
         break;
     case 9:
          cout<<"Brzina vrtnje sinkronog stroja";
-         cout<<""<<;
+         cout<<"Upisite vrijednosti"<<;
         cin>>a;
         cout<<"Iznos u kunama je: "<<a*7.56;
         break;
     case 10:
          cout<<"Klizanje asinkronog motora";
-         cout<<""<<;
+         cout<<"Upisite vrijednosti"<<;
         cin>>a;
         cout<<"Iznos u funtama je: "<<(a*7.02)/9.01;
         break;

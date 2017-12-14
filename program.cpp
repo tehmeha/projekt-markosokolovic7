90777dc98936ea238ce0d0cc8310224986ad0ab8
markosokolovic7 <msokolovic21@gmail.com>
2017-12-14 10:19:59 +0100

    zavrseno-predajem program
    ima funkcije reference i onaj vrag sa zvjezdicom,rekli ste da je to za 4 :)

--- program.cpp
+++ program.cpp
@@ -3,49 +3,49 @@
 using namespace std;
 double ohmov_zakon(double U,double R)
 {
-      return ohmov_zakon=U/R;
+      return U/R;
 }
 double snaga(double R,double I)
 {
-    return snaga=I*I*R;
+    return I*I*R;
 }
-double energija_rad(double P,double t)
+void energija_rad(double P,double t,double &energija)
 {
-    return energija_rad=P*t;
+    energija= P*t;
 }
-double veza_napona(double U,double d)
+void veza_napona(double U,double d, double *veza )
 {
-    return veza_napona=U/d;
+    *veza = U/d;
 }
 int main()
 {
-    cout<<"1.- Ohmov zakon "<<endl;
-    cout<<"2.-Ukupni otpor paralelno spojenih otpora   "<<endl;
-    cout<<"3.-Snaga "<<endl;
-    cout<<"4.-Energija,rad "<<endl;
-    cout<<"5.-Veza napona i jakos elektricnog polja "<<endl;
+    cout<<"1 - Ohmov zakon "<<endl;
+    cout<<"2 - Ukupni otpor paralelno spojenih otpora   "<<endl;
+    cout<<"3 - Snaga "<<endl;
+    cout<<"4 - Energija,rad "<<endl;
+    cout<<"5 - Veza napona i jakos elektricnog polja "<<endl;
 
-    double odabir;
+    int odabir;
     cout<<"Odaberite broj 1-5 s odgovarajucom formulom!"<<endl;
     cin>>odabir;
-    int U,R,R1,R2,Ruk,I,struja,Ukupni_otpor,P,t,e_r,d,veza_napona_i_jakost,snagaP;
-     switch(odabir)
+    double U,R,R1,R2,Ruk,I,struja,Ukupni_otpor,P,t,e_r,d,veza_napona_i_jakost,snagaP,energija;
+    switch(odabir)
     {
     case 1:
         cout<<"Ohmov zakon"<<endl;
-        cout<<"Upisite vrijednosti napona(U) "<<;
+        cout<<"Upisite vrijednosti napona(U) ";
         cin>>U;
-        cout<<"Upisite vrijednosti otopra(R)"<<;
+        cout<<"Upisite vrijednosti otopra(R): ";
         cin>>R;
         struja=ohmov_zakon(U,R);
         cout<<"Struja zadanih vrijednosti je: "<<struja<<endl;
         break;
     case 2:
-         cout<<"Ukupni otpor 2 paralelno spojenih otpora  "<endl;
-         cout<<"Upisite vrijednosti dva otpornika: "<<;
-        cin>>R1<<R2;
+         cout<<"Ukupni otpor 2 paralelno spojenih otpora  "<<endl;
+         cout<<"Upisite vrijednosti dva otpornika: ";
+        cin>>R1>>R2;
         Ruk=1/R1+1/R2;
-        Ruk=Ukupni_otpor;
+        Ukupni_otpor=Ruk;
         Ukupni_otpor=1/Ruk;
         cout<<"Otpor zadanih vrijednosti je: "<<Ukupni_otpor<<endl;
         break;
@@ -64,8 +64,8 @@ int main()
          cin>>P;
          cout<<"Upisite vrijednosti vremena u sekundama: ";
          cin>>t;
-         e_r=energija_rad(P,t);
-        cout<<"Izracunata energija(rad) je: "<<e_r;
+         energija_rad(P,t, energija);
+        cout<<"Izracunata energija(rad) je: "<<energija;
         break;
     case 5:
         cout<<"Veza napona i jakosti elektriènog polja"<<endl;
@@ -73,11 +73,12 @@ int main()
         cin>>U;
         cout<<"Upisite vrijednosti promjera(d): ";
         cin>>d;
-        veza_napona_i_jakost=veza_napona(U,d);
+        veza_napona(U,d, &veza_napona_i_jakost);
         cout<<"Izracunataveza napona i jakost je: "<<veza_napona_i_jakost;
         break;
     default:
         cout<<"Upisujes samo brojeve od 1-5 kretencino blesava"<<endl;
     }
+    cout<<endl;
     return 0;
 }
